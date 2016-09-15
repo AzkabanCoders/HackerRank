5573d7ab7a069d5243f46c781f4c0445ddef4a04
Emilio Aiolfi <emilio.neto@walmart.com>
2016-09-15 12:42:25 -0300

    Add Gem Stones Solutions

---
+++ GemStones/solution-E6.js
@@ -0,0 +1,23 @@
+// Check Fiddle
+// https://jsfiddle.net/EmilioAiolfi/4L5kzooa/
+
+function processData(input) {
+    console.log( new Set(input.toLowerCase()
+        .split('\n')
+        .slice(1)
+        .map(rock => rock.split(''))
+        .reduce((prev, curr) => curr.filter(element => prev.includes(element)))
+    ).size );
+}
+
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+    processData(_input);
+});
